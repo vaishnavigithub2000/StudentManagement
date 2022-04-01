1801da019cfaa889f059ae4f1b0cbbc87d80235c
vaishnavigithub2000 <vaishnaviapotikar2000@gmail.com>
2022-04-01 15:09:29 +0530

    pattern programming

---
+++ src/patternprogramming/p3.java
@@ -0,0 +1,24 @@
+package patternprogramming;
+
+public class p3 {
+
+        public static void main(String[] args) {
+            int n=5;
+
+            for (int i=1;i<=n;i++){
+
+                for(int j=1;j<=n;j++){
+                    if(j<=i) {
+                        System.out.print(i);
+                    }else {
+                        System.out.print(1);
+                    }
+                }
+                System.out.println();
+
+            }
+
+        }
+    }
+
+
---
+++ src/patternprogramming/p4.java
@@ -0,0 +1,17 @@
+package patternprogramming;
+
+public class p4 {
+
+        public static void main(String[] args) {
+            int n=5;
+
+            for (int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    System.out.print(n-i);
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
