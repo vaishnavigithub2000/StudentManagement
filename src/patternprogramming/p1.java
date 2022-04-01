08792102766cd6c20a24ca54277104c156d14301
vaishnavigithub2000 <vaishnaviapotikar2000@gmail.com>
2022-04-01 14:57:52 +0530

    pattern programming

---
+++ src/patternprogramming/p1.java
@@ -0,0 +1,18 @@
+package patternprogramming;
+
+public class p1 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(i==j){
+                    System.out.print(i);
+                }else {
+
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
