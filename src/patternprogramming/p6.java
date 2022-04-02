15ee27a63a84db6c28a095eb134bf8b953740a62
vaishnavigithub2000 <vaishnaviapotikar2000@gmail.com>
2022-04-02 11:22:27 +0530

    pattern programming

---
+++ src/patternprogramming/p6.java
@@ -0,0 +1,26 @@
+package patternprogramming;
+
+public class p6 {
+    public static void main(String[] args) {
+        int rows=5;//for number of rows
+        for (int i=1;i<=rows;i++){
+int num;
+                if(i%2==0)
+                {
+                  num=0;
+                    for(int j=1;j<=rows;j++){
+                        System.out.print(num);
+                        num=(num==0)?1:0;
+                    }
+                }else {
+                       num=1;
+                    for(int j=1;j<=rows;j++){
+                        System.out.print(num);
+                        num=(num==0)?1:0;
+                    }
+                }
+            System.out.println();
+            }
+        }
+    }
+
